ef3e19c7eeb98cecb2a3515d18ec057ee0fbd3d9
Israel Solomon Etuk <solomon.isy@gmail.com>
2022-12-27 13:49:20 +0100

    Updated all files

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ addTwoHome = document.querySelector("#add-two-home"),
 addThreeHome = document.querySelector("#add-three-home"),
 addOneAway = document.querySelector("#add-one-away"),
 addTwoAway = document.querySelector("#add-two-away"),
-addThreeAway = document.querySelector("#add-three-away")
+addThreeAway = document.querySelector("#add-three-away"),
+resetGameBtn = document.querySelector("#reset-game-btn")
 
 let addOne = () => countBoard1.textContent = homeCount += 1
 addOneHome.addEventListener("click", addOne)
 
-addTwoHome.addEventListener("click", function(){
-    countBoard1.textContent = homeCount += 2    
-})
-addThreeHome.addEventListener("click", function(){
-    countBoard1.textContent = homeCount += 3    
-})
+let addTwo = () => countBoard1.textContent = homeCount += 2
+addTwoHome.addEventListener("click", addTwo)
+
+let addThree = () => countBoard1.textContent = homeCount += 3
+addThreeHome.addEventListener("click", addThree)
+
 addOneAway.addEventListener("click", function(){
     countBoard2.textContent = awayCount += 1    
 })
@@ -26,7 +27,12 @@ addTwoAway.addEventListener("click", function(){
 addThreeAway.addEventListener("click", function(){
     countBoard2.textContent = awayCount += 3    
 })
-
+resetGameBtn.addEventListener("click", function(){
+    countBoard1.textContent = 0;
+    countBoard2.textContent = 0;
+    homeCount = 0;
+    awayCount = 0;
+})
 
 
 
